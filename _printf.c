d3045845ecfea0bc3a976c36e4d6ae6ac5bc68d4
geno <yvesgeno@outlook.com>
2023-04-22 23:48:07 +0200

    correction

--- _printf.c
+++ _printf.c
@@ -2,6 +2,48 @@
 #include <stdarg.h>
 #include <unistd.h>
 /**
+ * print_char - function to print char
+ * @args: argument to printf
+ * @j: number of printed charcters
+ * Return: void
+ */
+void print_char(va_list args, int *j)
+{
+	char c;
+
+	c = (char) va_arg(args, int);
+	write(1, &c, 1);
+	(*j)++;
+}
+/**
+ * print_string - function to print string
+ * @args: argument to printf
+ * @j: number of printed charcters
+ * Return: void
+ */
+void print_string(va_list args, int *j)
+{
+	char *s;
+
+	s = va_arg(args, char *);
+	while (*s)
+	{
+		write(1, s, 1);
+		s++;
+		(*j)++;
+	}
+}
+/**
+ * print_percent - function to print percent
+ * @j: number of printed charcters
+ * Return: void
+ */
+void print_percent(int *j)
+{
+	write(1, "%", 1);
+	(*j)++;
+}
+/**
  * _printf - function that works like printf function
  * @format:  is a character string
  * Return: number of charcters printed
@@ -9,9 +51,7 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int j;
-	char c;
-	char *s;
+	int j = 0;
 
 	va_start(args, format);
 	while (*format)
@@ -22,33 +62,16 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-					{
-					c = (cahr) va_arg(args, int);
-					write(1, &c, 1);
-					j++;
+					print_char(args, &j);
 					break;
-					}
 				case 's':
-					{
-						s = va_arg(args, char *);
-						while (*s)
-						{
-							write(1, s, 1);
-							s++;
-							j++;
-						}
-						break;
-					}
+					print_string(args, &j);
+					break;
 				case '%':
-					{
-						write(1, "%", 1);
-						j++;
-						break;
-					}
+					print_percent(&j);
+					break;
 				default:
-					{
-						return (-1);
-					}
+					return (-1);
 			}
 		}
 		else
