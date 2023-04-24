acd3ce648c8d1d363821f931571dd2fa2f959c73
geno <yvesgeno@outlook.com>
2023-04-24 20:24:29 +0200

    correction

--- functions.c
+++ functions.c
@@ -1,125 +1,39 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
-/**
- * count - function that counts number of digits
- * @n: integer to count
- * Return: number of digits in an int
- */
-int count(int n)
-{
-	int j = 0;
-
-	if (n < 0)
-	{
-		j++;
-		n = -n;
-	}
-
-	if (n == 0)
-		j++;
-	else
-	{
-		while (n > 0)
-		{
-			j++;
-			n /= 10;
-		}
-	}
-	return (j);
-}
-/**
- * print_integer - function that prints an integer argument
- * @args: va_list containing the integer argument
- * Return: number of characters printed
- */
-int print_integer(va_list args)
-{
-	int arg = va_arg(args, int);
-	int n = arg;
-	int digits = count(arg);
-	int sign = 0;
-	char *buffer;
-	int i;
-	int bytes_written;
-
-	if (n < 0)
-	{
-		sign = 1;
-		n = -n;
-	}
-	buffer = malloc(digits + sign + 1);
-	if (buffer == NULL)
-		return (-1);
-	i = sign;
-	if (sign)
-		buffer[0] = '-';
-	while (n > 0)
-	{
-		int d;
-
-		d = n % 10;
-		n /= 10;
-		buffer[i++] = d + '0';
-	}
-	buffer[i] = '\0';
-
-	reverse_string(buffer + sign, i - sign);
-
-	if (sign)
-		bytes_written = write(1, buffer, digits);
-	else
-	{
-		write(1, buffer, digits);
-		bytes_written = digits;
-	}
-	free(buffer);
-	return (bytes_written);
-}
 /**
  * print_char - function to print char
- * @args: argument to printf
- * @j: number of printed charcters
- * Return: void
+ * @arg: argument to printf
+ * Return: number of printed charcters
  */
-void print_char(va_list args, int *j)
+int print_char(va_list arg)
 {
-	char c;
-
-	c = (char) va_arg(args, int);
-	write(1, &c, 1);
-	(*j)++;
+	_putchar(va_arg(arg, int));
+	return (1);
 }
 /**
  * print_string - function to print string
- * @args: argument to printf
- * @j: number of printed charcters
- * Return: void
+ * @arg: argument to printf
+ * Return: number of printed charcters
  */
-void print_string(va_list args, int *j)
+int print_string(va_list arg)
 {
-	char *s;
+	int i;
+	char *str;
 
-	s = va_arg(args, char *);
-	if (s == NULL)
-	{
-		s = "(null)";
-	}
-	while (*s)
-	{
-		write(1, s, 1);
-		s++;
-		(*j)++;
-	}
+	str = va_arg(arg, char *);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
 }
+
 /**
- * print_percent - function to print percent
- * @j: number of printed charcters
- * Return: void
+ * print_percent - Prints a percent symbol
+ * @arg: arguments to printf
+ * Return: number of printed strings.
  */
-void print_percent(int *j)
+int print_percent(__attribute__((unused))va_list arg)
 {
-	write(1, "%", 1);
-	(*j)++;
+	_putchar('%');
+	return (1);
 }
--- main.h
+++ main.h
@@ -3,16 +3,24 @@
 
 #include <stdarg.h>
 #include <stdlib.h>
-int _printf(const char *format, ...);
-int count(int n);
-void print_int(int n);
-int _putchar(char c);
-int print_integer(va_list args);
-void print_char(va_list args, int *j);
-void print_string(va_list args, int *j);
-void print_percent(int *j);
-void print_percent(int *j);
-void reverse_string(char *str, int len);
 
+/**
+ * struct spcf - selct the collect function based on specifier
+ * @spc: specifier provided
+ * @f: function to use to print
+ */
+struct spcf
+{
+	char *spc;
+	int (*f)(va_list);
+};
+typedef struct spcf selct;
+
+int selector(const char *format, selct func[], va_list args);
+int print_char(va_list list);
+int print_string(va_list list);
+int print_percent(va_list list);
+int _putchar(char c);
+int _printf(const char *format, ...);
 
 #endif
